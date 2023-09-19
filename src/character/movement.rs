0393cb3f55a260c944cf23941ae0450222eba2d0
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-09-19 20:24:23 +0200

    remove hard speed limit

--- src/character/movement.rs
+++ src/character/movement.rs
@@ -16,7 +16,6 @@ impl Plugin for CharacterMovementPlugin {
                     update_movement_force,
                     update_corrective_force,
                     move_character,
-                    limit_character_speed,
                 )
                     .chain(),
                 stop_running_if_no_movement_input,
@@ -77,28 +76,6 @@ fn move_character(mut characters: Query<(&Character, &mut ExternalForce)>) {
     }
 }
 
-// TODO: can probably be removed once the character's 'active correcting force' is implemented
-fn limit_character_speed(mut characters: Query<(&mut Velocity, &Character, &CharacterConfig)>) {
-    for (mut velocity, character, config) in
-        characters
-            .iter_mut()
-            .filter(|(velocity, character, config)| {
-                is_over_max_speed(
-                    flatten_vector(velocity.linvel),
-                    config.get_movement_speed(character.is_running),
-                )
-            })
-    {
-        // bad to have hardcoded value but I'm pretty sure this system is temporary
-        let slowdown_factor = 0.2;
-
-        let max_speed = config.get_movement_speed(character.is_running);
-        let fraction_over_max = (flatten_vector(velocity.linvel).length() - max_speed) / max_speed;
-
-        velocity.linvel *= 1.0 - fraction_over_max * slowdown_factor;
-    }
-}
-
 fn stop_running_if_no_movement_input(mut characters: Query<&mut Character>) {
     for mut character in characters
         .iter_mut()
@@ -137,19 +114,6 @@ fn draw_gizmos(characters: Query<(&Character, &GlobalTransform, &Velocity)>, mut
 }
 
 // Utilities
-
-fn is_over_max_speed(velocity: Vec3, max_speed: f32) -> bool {
-    velocity.length() > max_speed
-}
-
-/// Resets the y value of the given vector to 0.0
-fn flatten_vector(vector: Vec3) -> Vec3 {
-    Vec3 {
-        x: vector.x,
-        y: 0.0,
-        z: vector.z,
-    }
-}
 
 fn looking_towards(direction: Vec3, up: Vec3) -> Quat {
     let back = -direction.try_normalize().unwrap_or(Vec3::NEG_Z);
